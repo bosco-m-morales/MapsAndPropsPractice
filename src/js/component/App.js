ce264125c7cc19404cbdd1288b26c1ea6bfd1e7f
Bosco Moises Morales <bmoises.morales@gmail.com>
2020-03-17 02:38:26 +0000

    latest update

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from "react";
-// import { Card } from "./Card";
+import { Card } from "./Card";
+
+let list = [
+	{
+		imageUrl:"https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg",
+		title: "Mr. Potter",
+        phone: "305-604-3769",
+        contactme:""
+	},
+	{
+		imageUrl:
+			"https://www.petsworld.in/blog/wp-content/uploads/2014/09/cute-kittens.jpg",
+        title: "Mr. & Mr. Cash",
+        phone:"305-619-2607",
+        contactme:""
+	},
+	{
+		imageUrl:
+			"https://savannahcatbreed.com/wp-content/uploads/2018/04/MilesF3John-1200x1095.jpg",
+        title: "Ms. Diva",
+        phone:"305-619-1015",
+        contactme:""
+	}
+];
+
 
 export function App() {
-	const [list, setList] = useState([]);
+    return(
+        {list.map(item,index) => {
+            return(
+                <Card
+                    key:{index}
+                    imageUrl: {}
 
-	useEffect(() => {
-		fetch("https://swapi.co/api/starships", {
-			method: "GET",
-			cache: "no-cache"
-		})
-			.then(resp => resp.json())
-			.then(data => {
-				console.log("list", data.results);
-				setList(data.results);
-			});
-	}, []);
 
-	return (
-		<div className="card m-3" style={{ width: "300px" }}>
-			{" "}
-			TESTINGGGGG{" "}
-		</div>
-	);
+
+            )
+        }}
+    )
+
 }
+	
